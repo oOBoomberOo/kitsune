cba11af1ac34e4c450f5e0eca7f71be26876fd41
oOBoomberOo <boomberisalreadytaken@gmail.com>
2024-02-25 05:51:37 +0900

    don't try to connect to database at build time

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,13 +1,16 @@
 import { Surreal } from 'surrealdb.js';
 import { env } from "$env/dynamic/private";
+import { building } from '$app/environment';
 
 export const surreal = new Surreal();
 
-await surreal.connect(env.SURREAL_URL);
+if (!building) {
+	await surreal.connect(env.SURREAL_URL);
 
-await surreal.signin({
-	username: env.SURREAL_NAME,
-	password: env.SURREAL_PASS,
-	database: env.SURREAL_DB,
-	namespace: env.SURREAL_NS
-});
+	await surreal.signin({
+		username: env.SURREAL_NAME,
+		password: env.SURREAL_PASS,
+		database: env.SURREAL_DB,
+		namespace: env.SURREAL_NS
+	});
+}
