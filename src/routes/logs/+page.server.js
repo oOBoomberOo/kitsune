4dcd521e3e3b8bdfdd00b6abb6dad385cbf7b308
oOBoomberOo <boomberisalreadytaken@gmail.com>
2024-06-07 04:01:03 +0900

    disabled logs

--- src/routes/logs/+page.server.js
+++ src/routes/logs/+page.server.js
@@ -1,6 +1,6 @@
-import { surreal } from "$lib/database";
+// import { surreal } from "$lib/database";
 
 export async function load() {
-	let [logs] = await surreal.query(`select *, array::first(<-wrote<-trackers.id) as tracker from logs order by created_at desc fetch tracker`);
-	return { logs };
+	// let [logs] = await surreal.query(`select *, array::first(<-wrote<-trackers.id) as tracker from logs order by created_at desc fetch tracker`);
+	return { logs: [] };
 }
